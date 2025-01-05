e1c13be15c0b97464cd065ecb660696925eb16b9
Dev1 <Dev1@gmail.com>
2025-01-05 15:57:18 +0530

    Test.java added by TL in Branch1

---
+++ DemoProj1/src/main/java/com/nt/test/Test.java
@@ -0,0 +1,11 @@
+package com.nt.test;
+
+public class Test {
+	public void check1() {
+		System.out.println("Test.check()");
+	}
+	
+	public void check2() {
+		System.out.println("Test.check2()");
+	}
+}
